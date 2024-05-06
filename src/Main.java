aef02b4d6d6dc162eda66127330e54a4a8d48c52
MariaM <myuller.masha@gmail.com>
2024-05-06 14:46:39 +0300

    Домашка в ветке вариант 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,58 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("задача 1!");
+
+        System.out.println("Задача 1");
+        int[] weights = new int[]{1, 2, 3};
+        double[] weightsTwo = {1.57, 7.654, 9.986};
+        int[] myArray = {52, 62, 32, 12, 42};
+
+        System.out.println("Задача 2");
+        for (int i = 0; i < weights.length; i++) {
+            if (i == weights.length - 1) {
+                System.out.println(weights[i]);
+                break;
+            }
+            System.out.print(weights[i] + ", ");
+        }
+        for (int i = 0; i < weightsTwo.length; i++) {
+            if (i == weightsTwo.length - 1) {
+                System.out.println(weightsTwo[i]);
+                break;
+            }
+            System.out.print(weightsTwo[i] + ", ");
+        }
+        for (int i = 0; i < myArray.length; i++) {
+            if (i == myArray.length - 1) {
+                System.out.println(myArray[i]);
+                break;
+            }
+            System.out.print(myArray[i] + ", ");
+        }
+
+        System.out.println("Задача 3");
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(weights[i] + ", ");
+        }
+        System.out.println(" ");
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(weightsTwo[i] + ", ");
+        }
+        System.out.println(" ");
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(myArray[i] + ", ");
+        }
+        System.out.println(" ");
+        System.out.println("Задача 4");
+        int[] weightsOne = {1, 2, 3};
+        for (int i = 0; i < weightsOne.length; i++) {
+            if (weightsOne[i] % 2 != 0) {
+                weightsOne[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(weightsOne));
     }
 }
